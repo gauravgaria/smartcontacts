467b0df6f2ff879242b4c79b6fd64bdaf602044a
Gaurav Garia <gaurav.garia@outlook.com>
2021-10-25 10:47:40 +0530

    added client

--- services/user/src/main/java/com/intellibyte/smartcontacts/users/ui/controllers/UserController.java
+++ services/user/src/main/java/com/intellibyte/smartcontacts/users/ui/controllers/UserController.java
@@ -1,15 +1,38 @@
 package com.intellibyte.smartcontacts.users.ui.controllers;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.intellibyte.smartcontacts.users.ui.model.UserModel;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
+	
+	private Logger _logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status")
 	public String status() {
-		return "Users service running";
+		
+		return "Working on port " + env.getProperty("local.server.port");
+	}
+	
+	@PostMapping(value="/register", consumes="application/json", produces="application/json")
+	public UserModel registerUser(@Valid @RequestBody UserModel user) {
+		_logger.info(user.getEmail());
+		
+		return user;
 	}
 }
